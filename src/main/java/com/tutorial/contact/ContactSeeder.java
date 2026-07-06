74cfb7454a3685e499d6289701130ef24b7de956
moein <moeinkolivand97@gmail.com>
2026-07-06 14:59:45 +0330

    remove main method from ContactSeeder

--- src/main/java/com/tutorial/contact/ContactSeeder.java
+++ src/main/java/com/tutorial/contact/ContactSeeder.java
@@ -51,9 +51,4 @@ public class ContactSeeder {
         return String.format("(%03d) %03d-%04d", area, prefix, line);
     }
 
-    // Quick demo
-    public static void main(String[] args) {
-        List<Contact> contacts = generateContacts(10);
-        contacts.forEach(System.out::println);
-    }
 }
